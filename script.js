c5a2ba48999c2a4ef485e7e0f993320189e7b3b1
joshchopin7 <126109111+joshchopin7@users.noreply.github.com>
2023-05-14 19:56:34 -0700

    finish game

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let scoreU = 0
 
+const score = document.getElementById('score')
+
+function finalScore() {
+    if (scoreU >= 3) {
+        alert("You are the CHAMPION!!!");
+    } 
+}
 // First lets set up a prompt to create an input for yourChoice
 
 
@@ -60,10 +67,10 @@ function gamePlay() {
 // First we get yourChoice, them itsChoice, them gamePlay
 
 
-function game() {
+function gameRock() {
     // yourChoice
-    let userInput = prompt("What do you choose??");
-    let yourChoice = userInput.toLowerCase()
+    
+    let yourChoice = 'rock'
 
     console.log(yourChoice.toLowerCase());
     // itsChoice
@@ -108,30 +115,144 @@ function game() {
                     itsChoice == 'rock') {
             console.log('YOU LOSE!!')
         }
-        console.log(scoreU);
+        score.textContent= (scoreU);
     }
-    gamePlay()
+    gamePlay();
+    finalScore();
+}
+
+function gamePaper() {
+    // yourChoice
+    
+    let yourChoice = 'paper'
+
+    console.log(yourChoice.toLowerCase());
+    // itsChoice
+    let numChoice = Math.floor(Math.random() * 10);
+    function generateSelection() {
+        if (numChoice <= 3) {
+            return ('rock');
+        } else if (numChoice > 3 && numChoice < 7) {
+            return ('paper');
+        } else {
+            return ('scissors');
+        }
+    }
+    let itsChoice = generateSelection();
+    console.log(itsChoice);
+    // gameplay()
+    function gamePlay() {
+        if (yourChoice === itsChoice) {
+            console.log('DRAW!!');
+        } else if(yourChoice == 'rock' &&
+                    itsChoice == 'scissors') {
+            console.log('YOU WIN!!'); 
+            // A WIN!
+             scoreU = scoreU + 1;
+        } else if(yourChoice == 'rock' &&
+                    itsChoice == 'paper') {
+            console.log('YOU LOSE!!');
+        } else if(yourChoice == 'paper' &&
+                    itsChoice == 'rock') {
+            console.log('YOU WIN!!');
+            // A WIN!
+            scoreU = scoreU + 1;
+        } else if(yourChoice == 'paper' &&
+                    itsChoice == 'scissors') {
+            console.log('YOU LOSE!!')
+        } else if(yourChoice == 'scissors' &&
+                    itsChoice == 'paper') {
+            console.log('YOU WIN!!');
+            // A WIN!
+            scoreU = scoreU + 1;
+        } else if(yourChoice == 'scissors' &&
+                    itsChoice == 'rock') {
+            console.log('YOU LOSE!!')
+        }
+        score.textContent= (scoreU);
+    }
+    gamePlay();
+    finalScore();
+}
+
+function gameScissors() {
+    // yourChoice
+    
+    let yourChoice = 'scissors'
+
+    console.log(yourChoice.toLowerCase());
+    // itsChoice
+    let numChoice = Math.floor(Math.random() * 10);
+    function generateSelection() {
+        if (numChoice <= 3) {
+            return ('rock');
+        } else if (numChoice > 3 && numChoice < 7) {
+            return ('paper');
+        } else {
+            return ('scissors');
+        }
+    }
+    let itsChoice = generateSelection();
+    console.log(itsChoice);
+    // gameplay()
+    function gamePlay() {
+        if (yourChoice === itsChoice) {
+            console.log('DRAW!!');
+        } else if(yourChoice == 'rock' &&
+                    itsChoice == 'scissors') {
+            console.log('YOU WIN!!'); 
+            // A WIN!
+             scoreU = scoreU + 1;
+        } else if(yourChoice == 'rock' &&
+                    itsChoice == 'paper') {
+            console.log('YOU LOSE!!');
+        } else if(yourChoice == 'paper' &&
+                    itsChoice == 'rock') {
+            console.log('YOU WIN!!');
+            // A WIN!
+            scoreU = scoreU + 1;
+        } else if(yourChoice == 'paper' &&
+                    itsChoice == 'scissors') {
+            console.log('YOU LOSE!!')
+        } else if(yourChoice == 'scissors' &&
+                    itsChoice == 'paper') {
+            console.log('YOU WIN!!');
+            // A WIN!
+            scoreU = scoreU + 1;
+        } else if(yourChoice == 'scissors' &&
+                    itsChoice == 'rock') {
+            console.log('YOU LOSE!!')
+        }
+        score.textContent= (scoreU);
+    }
+    gamePlay();
+    finalScore();
 }
 
 // Create Function for the FINAL SCORE
 
-function finalScore() {
-    if (scoreU >= 3) {
-        alert("You are the CHAMPION!!!");
-    } else {
-        alert("YOU LOSE!!");
-    }
-}
-
-game();
-game();
-game();
-game();
-game();
-finalScore();
 
 
 
+// add an event listener for the buttons
 
+const rock = document.getElementById('rock')
+
+rock.addEventListener("click", function() {
+    gameRock();
+    
+});
+
+const paper = document.getElementById('paper');
+
+paper.addEventListener('click', function() {
+    gamePaper();
+})
+
+const scissors = document.getElementById('scissors');
+
+scissors.addEventListener('click', function() {
+    gameScissors();
+})
 
 
